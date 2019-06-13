01a27fce596c6a715a1045bf69f1a2901060ea36
YWANG <wangyang1712@gmail.com>
2019-06-13 17:16:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# test-gmail
+https://www.mkyong.com/java/javamail-api-sending-email-via-gmail-smtp-example/
---
+++ src/main/java/SendEmailSSL.java
@@ -0,0 +1,48 @@
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendEmailSSL {
+
+    public static void main(String[] args) {
+
+        final String username = "xxxxxx@gmail.com";
+        final String password = "xxxxxx";
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        Session session = Session.getInstance(prop,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("xxxxxx@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("xxxxxx@gmail.com")
+            );
+            message.setSubject("Testing Gmail SSL");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
